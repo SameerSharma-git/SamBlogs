b0246d7d133d010b08e478adf7d579da076cec8d
SameerSharma-git <sameersharm1234@gmail.com>
2025-07-31 12:35:35 +0530

    Update page.js - fixed link issue

--- app/(pages)/blogpost/[slug]/page.js
+++ app/(pages)/blogpost/[slug]/page.js
@@ -41,7 +41,7 @@ export default async function BlogPostPage({ params }) {
       <div className="max-w-4xl mx-auto rounded-lg shadow-xl overflow-hidden p-6 md:p-10 bg-blogpost">
 
         {/* Back to Blogs Link */}
-        <Link href="/explore-blogs" passHref>
+        <Link href="/exploreblogs" passHref>
           <button className="inline-flex items-center text-blue-500 hover:text-blue-700 transition duration-300 mb-6">
             <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path></svg>
             Back to all blogs
